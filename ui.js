79f5d6976286c4e5a2ec6cd5c7e90a0054d0bf0c
Arturo Baldo <baldoarturo@gmail.com>
2019-06-27 13:31:06 -0300

    Second Commit

--- ui.js
+++ ui.js
@@ -1,12 +1,20 @@
-const UNIT_SELECTOR_TEMPLATE = '<p><a class="button unit-button" id="UNIT-ID">UNIT_TEXT</a></p>';
+const UNIT_TYPE_SELECTOR_TEMPLATE = '<p><a class="button" id="UNIT-ID">UNIT_TEXT</a></p>';
+const UNIT_SELECTOR_TEMPLATE = '<p><a class="button" id="UNIT-ID">UNIT_TEXT</a></p>';
 
 // Get all kinds of units and their units
-// Add kinds of units to the top bar
+// Add kinds of units to the selector
 function loadUnits() {
-    var units = Qty.getKinds();
-    units.forEach(unit => {
-        unit_type = UNIT_SELECTOR_TEMPLATE.replace('UNIT-ID',unit).replace('UNIT_TEXT',unit.replace('_',' '));
-        $('#units-types-container-column').append(unit_type);
+    var unit_types = Qty.getKinds();
+    unit_types.forEach(unit_type => {
+        // Populate unit type selector
+        unit_type_html = UNIT_TYPE_SELECTOR_TEMPLATE.replace('UNIT-ID', unit_type).replace('UNIT_TEXT', unit_type.replace('_', ' '));
+        $('#units-types-container-column').append(unit_type_html);
+        // Populate source and destination unit selector
+        units = Qty.getUnits(unit_type);
+        units.forEach(unit => {
+            $('#src-unit').append(unit);
+            $('#dst-unit').append(unit);
+        });
     });
 }
 
